36a3e8554ca1510990b86f154404691d0193cea2
Harcyldo Winkelmann <harcyleo@gmail.com>
2023-11-01 10:57:25 -0400

    Salvando lista de tarefas em localStorage | Finalizando projeto

--- scripts.js
+++ scripts.js
@@ -13,14 +13,21 @@ const todosApp = {
             if(this.newTodo.text) {
                 this.toDos.push(this.newTodo);
                 this.newTodo = { done: false };
+                localStorage.setItem("ToDo's", JSON.stringify(this.toDos));
             } else {
                 window.alert('Você precisa fornecer uma descrição para sua nova tarefa!');
             }
         },
         clearTodo: function() {
             this.toDos = []
+        },
+        storeTodos: function() {
+            localStorage.setItem("ToDo's", JSON.stringify(this.toDos));
         }
     },
+    created() {
+        this.toDos = localStorage.getItem("ToDo's") ? JSON.parse(localStorage.getItem("ToDo's")) : this.toDos;
+    }
 };
 
 Vue.createApp(todosApp).mount('#app');

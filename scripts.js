d9589fa0675fafc24b2ca64681c38430b5a0fe1e
Harcyldo Winkelmann <harcyleo@gmail.com>
2023-11-01 09:25:04 -0400

    Utilizando as diretivas v-for e v-bind para a exibição dos dados dinamicamente

--- scripts.js
+++ scripts.js
@@ -1,9 +1,24 @@
+var todos = [
+    {
+        text: 'Aprender HTML, CSS e Javascript',
+        done: true
+    },
+    {
+        text: 'Aprender o básico de Vue JS',
+        done: true
+    },
+    {
+        text: 'Completar o desafio de Vue JS com excelência',
+        done: false
+    }
+];
+
 const todosApp = {
     data() {
         return {
-            toDos: []
-        }
+            toDos: window.todos
+        };
     }
 };
 
-Vue.createApp(todosApp).mout('#app')
+Vue.createApp(todosApp).mount('#app');
